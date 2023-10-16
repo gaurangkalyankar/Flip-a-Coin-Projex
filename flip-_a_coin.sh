44f33ede46446be77b5de5a05f846d3b52918f73
Gaurang Kalyankar <77961397+gaurangkalyankar@users.noreply.github.com>
2023-10-17 01:06:57 +0530

    Update flip-_a_coin.sh

--- flip-_a_coin.sh
+++ flip-_a_coin.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Define the character set
-charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?/[]{}"
+# Generate a random number (0 or 1)
+random_number=$((RANDOM % 2))
 
-# Function to generate a random password
-generate_password() {
-  for ((i = 0; i < 12; i++)); do
-    random_index=$((RANDOM % 84))
-    password+=${charset:$random_index:1}
-  done
-  echo "$password"
-}
+# Define the outcomes
+outcomes=("Heads" "Tails")
 
-# Generate and display the password
-generated_password=$(generate_password)
-echo "Generated Password: $generated_password"
+# Display the result
+echo "Coin flipped: ${outcomes[random_number]}"
